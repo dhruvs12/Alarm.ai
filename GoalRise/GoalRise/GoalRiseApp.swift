5fa9f702dc11bf79dda96e98cf4c36c4208a69cf
dhruvs12 <77699974+dhruvs12@users.noreply.github.com>
2025-01-09 17:57:35 -0800

    xcode

---
+++ GoalRise/GoalRise/GoalRiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalRiseApp.swift
+//  GoalRise
+//
+//  Created by Dhruv on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GoalRiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoalRise/GoalRiseTests/GoalRiseTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoalRiseTests.swift
+//  GoalRiseTests
+//
+//  Created by Dhruv on 1/9/25.
+//
+
+import Testing
+@testable import GoalRise
+
+struct GoalRiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
